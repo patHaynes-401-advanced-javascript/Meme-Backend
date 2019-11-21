1e01c61e31e5d8855fd08d3980998522fca60aee
Pat Haynes <pathaynes32@gmail.com>
2019-11-20 17:43:39 -0800

    environment set up

---
+++ test/app.test.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/model/User');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can signup a new user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'test', password: 'password1234' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'test'
+        });
+      });
+  });
+
+});
